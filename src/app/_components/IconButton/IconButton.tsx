9f83f37d2862120bd0544d08c2c170e18ce5da34
Niko Ivan <nixonivan@rambler.ru>
2024-08-12 09:42:36 +0300

    fix

--- src/app/_components/IconButton/IconButton.tsx
+++ src/app/_components/IconButton/IconButton.tsx
@@ -5,7 +5,6 @@ import { IClassNameProps } from '@bem-react/core';
 import { cn } from '@bem-react/classname';
 
 import './IconButton.scss';
-import { on } from 'events';
 
 type IconButtonProps = {
 	onClick?: (e: unknown) => void;
